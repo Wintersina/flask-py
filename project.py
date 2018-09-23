8c75806caea1c23090380bdfd4204dd6689308f4
Sina Serati <sina.serati@cigna.com>
2018-09-23 13:42:05 -0700

    add: adding templates and updating file project.py to handle new templates

--- project.py
+++ project.py
@@ -1,33 +1,72 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect, request, flash
 import os
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Resturant, MenuItem
+from database_setup import Base, Restaurant, MenuItem
 
-sql_lite_db = create_engine('sqlite:///resturantmenu.db')  # say what database
+sql_lite_db = create_engine('sqlite:///restaurantmenu.db')  # say what database
 Base.metadata.bind = sql_lite_db
 DBsession = sessionmaker(bind = sql_lite_db)
 session = DBsession()
 
 
 @app.route('/')
-@app.route('/restaurants/<int:resturant_id>/')
-def HelloWorld(resturant_id):
-    results = session.query(Resturant).all()
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
 
-    output = ""
-    menuItem = session.query(MenuItem).filter_by(resturant_id = resturant_id)
-    output+= "<br><h2>Menu Items For %s : </h2> <br>" % menuItem.first().resturant.name
-
-    for i in menuItem:
-        output += "%s %s <br>"% (i.item_name,i.price)
-        output += "%s <br>"%i.description
+    restaurantResult = session.query(Restaurant).filter_by(id = restaurant_id)
+    menuItems = session.query(MenuItem).filter_by(restaurant_id = restaurant_id)
+    restaurantFinalResult = restaurantResult.one()
+    output = render_template('menu.html', restaurant=restaurantFinalResult, menuItems = menuItems)
 
     return output
 
+#Create a new menu item
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET','POST'])
+def newMenuItem(restaurant_id):
+    if request.method == 'POST':
+        newItem = MenuItem(item_name = request.form['name'],restaurant_id = restaurant_id)
+        session.add(newItem)
+        session.commit()
+        flash("Menu item created!")
+        output = redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+        return output
+    else:
+        output = render_template('newMenuItem.html', restaurant_id= restaurant_id)
+        return output
+
+# Task 2: Create route for editMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/', methods= ['GET','POST'])
+def editMenuItem(restaurant_id, menu_id):
+    editItem = session.query(MenuItem).filter_by(id = menu_id).one()
+    if request.method == 'POST':
+        editItem.item_name = request.form['name']
+        session.add(editItem)
+        session.commit()
+        flash("Menu item edited!!")
+        output = redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+        return output
+    else:
+        output = render_template('menuitemedit.html',restaurant_id= restaurant_id, menu_id=menu_id,i_name=editItem.item_name)
+        return output
+# Task 3: Create a route for deleteMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/' , methods= ['GET','POST'])
+def deleteMenuItem(restaurant_id, menu_id):
+    editItem = session.query(MenuItem).filter_by(id = menu_id).one()
+    if request.method == 'POST':
+        session.delete(editItem)
+        session.commit()
+        flash("Menu item DELETEEEEED!")
+        output = redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+        return output
+    else:
+        output = render_template('delete.html',restaurant_id= restaurant_id, menu_id=menu_id,i_name=editItem.item_name)
+        return output
+
 if __name__ == '__main__':
+    app.secret_key = "yum_yum_key"
     app.debug = True
     port = int(os.environ.get('PORT',5000))
     app.run(host = '0.0.0.0', port = port)
